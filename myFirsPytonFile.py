feca975a386311af1ce61b886d5bc052b0850aaf
Pavlin84 <58217926+Pavlin84@users.noreply.github.com>
2022-11-17 16:16:11 +0200

    fix

--- myFirsPytonFile.py
+++ myFirsPytonFile.py
@@ -5,6 +5,6 @@ streamlit.title('Snowflake Badge')
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑 Hard-Boiled Free-Range Egg'🍞)
+streamlit.text('🥑 Hard-Boiled Free-Range Egg 🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
