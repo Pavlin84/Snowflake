c5845e5908e6ebaa82112cbe3bfba5506bb3c1d1
Pavlin84 <58217926+Pavlin84@users.noreply.github.com>
2022-11-17 16:10:31 +0200

    fix text issue

--- myFirsPytonFile.py
+++ myFirsPytonFile.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('Snowflake Badge')
 
-streamlit.header(🥣'Breakfast Menu')
-streamlit.text(🥗 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🐔 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🥑'Hard-Boiled Free-Range Egg'🍞)
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑 Hard-Boiled Free-Range Egg'🍞)
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
