62864688d61564d9d2ff6e03f8a5a54229d5c648
Pavlin84 <58217926+Pavlin84@users.noreply.github.com>
2022-12-05 23:20:01 +0200

    Add request

--- myFirsPytonFile.py
+++ myFirsPytonFile.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('Snowflake Badge')
 
@@ -23,5 +24,9 @@ fruit_to_show = my_fruit_list.loc[fruit_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruit_to_show)
-streamlit.dataframe(my_fruit_list);
+streamlit.dataframe(my_fruit_list)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
