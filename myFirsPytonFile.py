daba4b6d4380b51beeb878725e87b1011e3f2d19
Pavlin Stoyanchov <pavlin.stoyanchov@blankfactor.com>
2022-12-05 23:32:12 +0200

    Normalize json

--- myFirsPytonFile.py
+++ myFirsPytonFile.py
@@ -30,3 +30,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+fruit_normalaized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(fruit_normalaized)
